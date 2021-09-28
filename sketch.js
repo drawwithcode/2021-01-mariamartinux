8a54350743c87f5aaf4be7bfbc1cdcc077ceaeb5
mariamartinux <mmariaester@gmail.com>
2021-09-28 17:52:43 +0200

    Generative artwork

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  angleMode(DEGREES);
+  background("#12FF05");
 }
 
 function draw() {
-  // put drawing code here
-  background("green");
+  noFill();
+
+  let myColor = lerpColor(color("black"), color("red"), frameCount / 2000);
+
+  stroke(myColor);
+  translate(width / 2, height / 2);
+  rotate(frameCount * 3);
+  scale(frameCount / 300);
+  rect(100, 20, 100, 30, 40, 60, 100, 80);
 }
